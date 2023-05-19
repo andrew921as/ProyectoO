6b917fc42a6c8e229c624d35d77d94c8d5bc97a7
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-05-19 18:42:11 -0500

    working on loging page (Jose, Valery)

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { useState } from 'react'
 import axios from 'axios'
+import { GrMailOption } from 'react-icons/Gr'
+import { AiFillUnlock } from 'react-icons/Ai'
 
 const Login = () => {
   const [email, setEmail] = useState('')
@@ -31,39 +33,57 @@ const Login = () => {
   }
 
   return (
-    <div className='flex justify-center items-center w-full h-screen bg-[url("/img/login/login.png")] bg-no-repeat bg-cover bg-center'>
+    <div className='flex  items-center w-full h-screen bg-[url("/img/login/login.png")] bg-no-repeat bg-cover bg-center'>
       {/* <img src='/img/login/login.png' alt='Fondo De Inicio de Sesión' /> */}
-      <form onSubmit={handleSubmit} className='w-full max-w-sm p-6 bg-sin_derechos text-amarillito rounded shadow-lg'>
-        <h2 className='text-2xl font-bold mb-6 text-center font-texto'>Iniciar sesión</h2>
-        <div className='mb-4'>
-          <label htmlFor='email' className='block mb-2 font-semibold'>
+      <form
+        onSubmit={handleSubmit}
+        className='w-full max-w-xl flex justify-center flex-col h-[800px] p-8 ml-20 bg-sin_derechos bg-opacity-75 text-amarillito text-left rounded shadow-lg'
+      >
+        <h2 className='text-7xl font-bold mb-6 text-center font-texto'>Iniciar sesión</h2>
+        <div className='flex mb-4'>
+          {/* <label htmlFor='email' className='block mb-2 font-semibold'>
             Email
-          </label>
+          </label> */}
+          <GrMailOption className='w-10 h-10 mr-2 mt-2 text-amarillito' />
           <input
             type='email'
             id='email'
+            placeholder='Email'
             value={email}
             onChange={handleEmailChange}
-            className='w-full p-2 border border-gray-300 rounded'
+            className='w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-3xl placeholder:font-light'
             required
           />
         </div>
-        <div className='mb-4'>
-          <label htmlFor='password' className='block mb-2 font-semibold'>
+        <div className='flex mb-4'>
+          {/* <label htmlFor='password' className='block mb-2 font-semibold'>
             Contraseña
-          </label>
+          </label> */}
+          <AiFillUnlock className='w-10 h-10 mr-2 mt-2 text-amarillito' />
           <input
             type='password'
             id='password'
+            placeholder='Contraseña'
             value={password}
             onChange={handlePasswordChange}
-            className='w-full p-2 border border-gray-300 rounded'
+            className='w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-3xl placeholder:font-light'
             required
           />
         </div>
-        <button type='submit' className='w-full p-2 bg-caca_clara text-amarillito rounded hover:bg-blue-600'>
-          Iniciar sesión
-        </button>
+        <div className='flex'>
+          <button
+            type='submit'
+            className='w-1/2 p-2 bg-caca_clara text-amarillito border-2 border-amarillito rounded hover:opacity-50'
+          >
+            Volver
+          </button>
+          <button
+            type='submit'
+            className='w-1/2 p-2 bg-moradito_palido text-amarillito border-2 border-amarillito rounded hover:opacity-50'
+          >
+            Ingresar
+          </button>
+        </div>
       </form>
     </div>
   )
