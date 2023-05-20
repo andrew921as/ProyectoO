c8d63284873879a85e580646ca7d65542be43498
Cesar-velasquez117 <106538584+Cesar-velasquez117@users.noreply.github.com>
2023-05-20 11:46:32 -0500

    Intro page with animations done (Cesar&Andres)

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,52 +1,77 @@
 'use client'
 import styles from './page.module.css'
+import Image from 'next/image';
 import { motion } from "framer-motion";
+
+//Images
+import cloudImg from "/public/img/nubesita.png"
+import registrarseButton from "/public/icons/home/registrarseButton.svg"
+import iniciarSButton from "/public/icons/home/iniciarSButton.svg"
 
 export default function Page() {
   return (
     <div
-      className="w-full h-full bg-[url('/img/home.jpg')] bg-cover bg-center grid text-center justify-center"
+      id={styles.mainHome}
+      className="w-full h-full grid text-center justify-center"
     >
       <motion.div 
-        className="absolute bg-[#50d71e] z-30 w-32 h-20"
+        className={styles.box}
         initial={{
           y:0,
           opacity:1
         }}
-        // animate={{
-        //   x:500,
-        //   y:[0,-300],
-        //   opacity:[0.5,0]
-        // }}
-        // transition={{
-        //   duration:5,
-        //   ease:"easeInOut"
-        // }}
-        />
+        animate={{
+          x:"-50vw",
+          y:[0,-300],
+          opacity:[0.5,0]
+        }}
+        transition={{
+          duration:5,
+          ease:"easeInOut"
+        }}
+        >
+          <Image
+          alt='Cloud'
+          src={cloudImg}
+          fill
+          />
+        </motion.div>
         <motion.div 
-        className="absolute bg-[#f02a2a] z-30 w-32 h-20"
+        className={styles.box2}
         initial={{
           y:0,
-          x:-1,
+          x:"50vw",
           opacity:1
         }}
-        // animate={{
-        //   x:-500,
-        //   y:[0,-300],
-        //   opacity:[0.5,0]
-        // }}
-        // transition={{
-        //   duration:5,
-        //   ease:"easeInOut"
-        // }}
-        />
-      <h1 id={styles.title} className="text-7xl mt-10 md:text-9xl"> OLIMPOCRAFT</h1>
+        animate={{
+          x:"100vw",
+          y:[0,-300],
+          opacity:[0.5,0]
+        }}
+        transition={{
+          duration:5,
+          ease:"easeInOut"
+        }}
+        >
+          <Image
+          alt='Cloud'
+          src={cloudImg}
+          fill
+          />
+        </motion.div>
+      <h1 id={styles.title} className="text-7xl mt-10 md:text-9xl "> OLIMPOCRAFT</h1>
       <div className='flex h-1/6 inset-x-0 bottom-0 absolute justify-center'>
-        <a className='relative'>
-          <img src='icons/home/registrarseButton.svg' alt='Volver button' />
+        <a className='relative md:scale-9 lg:scale-75'>
+          <Image 
+            src={registrarseButton} 
+            alt='registartse Button' 
+          />
         </a>    
-        <a className='relative'>
-          <img src='icons/home/iniciarSButton.svg' alt='Volver button' />
+        <a className='relative md:scale-9 lg:scale-75'>
+          <Image 
+            src={iniciarSButton} 
+            alt='Iniciar Sesion Button' 
+          />
         </a>
       </div>
     </div>
