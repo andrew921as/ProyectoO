8e1cfa9d157a2a191fda607b93b07e2547006c8c
Valery Molina <valerymolinalika@gmail.com>
2023-05-20 11:16:16 -0500

    Database modifications  (Valery, Jose)

--- backend/src/Models/User.js
+++ backend/src/Models/User.js
@@ -27,6 +27,16 @@ const userSchema = new mongoose.Schema({
 		required: false,
 		default: 'https://res.cloudinary.com/dy1xcx7kw/image/upload/v1623687229/olimpo/avatar-default.png',
 	},
+	progress: {
+		type: Number,
+		default: 0,
+		required: false,
+	},
+	points: {
+		type: Number,
+		default: 0,
+		required: false,
+	},
 	keys: [
 		{
 			type: mongoose.Schema.Types.ObjectId,
