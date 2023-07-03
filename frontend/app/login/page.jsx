b5e8f31af588043ba685af58adac3c2a3552006c
Andres Velasco <andresfelipecoronado@gmail.com>
2023-07-03 16:13:40 -0500

    Somechanges unused

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -7,7 +7,7 @@ import { apiUrl } from '@/config'
 import Link from 'next/link'
 import Swal from 'sweetalert2'
 import { provider } from '@/functions'
-import { getAuth, signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo } from "firebase/auth";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo, getRedirectResult } from "firebase/auth";
 
 // COMPONENTS
 import Loading from '@/components/elements/Loading'
@@ -23,6 +23,7 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [formHeight, setFormHeight] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
+  const [isGoogle, setIsGoogle] = useState(false)
   const router = useRouter()
 
   const handleEmailChange = (e) => {
@@ -83,44 +84,101 @@ const Login = () => {
   }
 
   //Inicio de sesion  con google
-  
+  const auth = getAuth();
+
   const handleGoogleLogin = async () => {
-    if (typeof window !== 'undefined') {
-    const auth = getAuth();
-    // Inicia el loading
-    setIsLoading(true)
-      const { profile } = await signInWithPopup(auth, provider)
-        .then((result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
-          // The signed-in user info.
-          const moreUser = getAdditionalUserInfo(result)
+    setIsGoogle(true)
+    signInWithRedirect(auth, provider);
+    //   if (typeof window !== 'undefined') {
+    //   const auth = getAuth();
+    //   // Inicia el loading
+    //   setIsLoading(true)
+    //     const { profile } = await signInWithPopup(auth, provider)
+    //       .then((result) => {
+    //         // This gives you a Google Access Token. You can use it to access the Google API.
+    //         const credential = GoogleAuthProvider.credentialFromResult(result);
+    //         const token = credential.accessToken;
+    //         // The signed-in user info.
+    //         const moreUser = getAdditionalUserInfo(result)
 
-          //Comentario
-          console.log(moreUser);
+    //         //Comentario
+    //         console.log(moreUser);
 
-          return moreUser
-          // IdP data available using getAdditionalUserInfo(result)
-          // ...
-        }).catch((error) => {
-          // Handle Errors here.
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // The email of the user's account used.
-          const email = error.customData.email;
-          // The AuthCredential type that was used.
-          const credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-          return error
-        });
+    //         return moreUser
+    //         // IdP data available using getAdditionalUserInfo(result)
+    //         // ...
+    //       }).catch((error) => {
+    //         // Handle Errors here.
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+    //         // The email of the user's account used.
+    //         const email = error.customData.email;
+    //         // The AuthCredential type that was used.
+    //         const credential = GoogleAuthProvider.credentialFromError(error);
+    //         // ...
+    //         return error
+    //       });
 
-      const data = {
-        name: profile.given_name,
-        last_name: profile.family_name,
-        email: profile.email,
-        password: profile.id,
-      }
+    //     const data = {
+    //       name: profile.given_name,
+    //       last_name: profile.family_name,
+    //       email: profile.email,
+    //       password: profile.id,
+    //     }
+    //   if (profile) {
+    //     axios
+    //       .post(`${apiUrl}/users/loginGoogle`, data)
+    //       .then((response) => {
+    //         console.log(response.data) // Maneja la respuesta del servidor según tus necesidades
+
+    //         // Detiene el loading
+    //         setIsLoading(false)
+
+    //         if (response.data.user) {
+    //           // Guarda el usuario en el localStorage
+    //           localStorage.setItem('user', JSON.stringify(response.data.user))
+    //         }
+    //         // Detiene el loading
+    //         setIsLoading(false)
+    //         // Envía al usuario a la página de inicio
+    //         router.push('/world')
+    //       })
+    //       .catch((error) => {
+    //         // console.error(error)
+
+    //         // Detiene el loading
+    //         setIsLoading(false)
+
+    //         // Alerta de error
+    //         Swal.fire({
+    //           title: 'Error!',
+    //           text: 'Ocurrió un error al iniciar sesión. Verifica tus credenciales.',
+    //           confirmButtonText: 'Aceptar',
+    //           buttonsStyling: false,
+    //           color: '#F4DFB0',
+    //           iconColor: '#F4DFB0',
+    //           background: '#8C6F4D',
+    //           iconHtml: '<img src="/icons/login/advertencia.svg" alt="error" class="w-20 h-20">',
+    //           customClass: {
+    //             popup: 'rounded-3xl',
+    //             container: 'rounded-xl',
+    //             title: 'text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
+    //             htmlContainer: 'text-amarillito text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
+    //             confirmButton: 'bg-moradito_palido text-amarillito font-texto text-xl p-4 rounded',
+    //           },
+    //         })
+    //       })
+    //   }
+    // }
+  }
+
+  const redirectWorld = async (profile) => {
+    const data = {
+      name: profile.given_name,
+      last_name: profile.family_name,
+      email: profile.email,
+      password: profile.id,
+    }
     if (profile) {
       axios
         .post(`${apiUrl}/users/loginGoogle`, data)
@@ -135,6 +193,7 @@ const Login = () => {
             localStorage.setItem('user', JSON.stringify(response.data.user))
           }
           // Detiene el loading
+          setIsGoogle(false)
           setIsLoading(false)
           // Envía al usuario a la página de inicio
           router.push('/world')
@@ -165,86 +224,104 @@ const Login = () => {
           })
         })
     }
-  }}
+  }
 
-  useEffect(() => {
-    const screenHeight = window.innerHeight
-    const calculatedHeight = screenHeight * 0.96
-    setFormHeight(calculatedHeight)
-    
-  }, [])
+useEffect(() => {
+  const screenHeight = window.innerHeight
+  const calculatedHeight = screenHeight * 0.96
+  setFormHeight(calculatedHeight)
 
-  
+  if (isGoogle && typeof window !== 'undefined') {
+    const fetchData = async () => {
+      setIsLoading(true)
+      const { profile } = await getRedirectResult(auth)
+        .then(async (result) => {
+          const credential = GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
+          const resultt = await signInWithCredential(auth, credential);
 
-  return (
-    <div
-      className='flex items-center w-full h-screen'
-      style={{
-        backgroundImage: `url("/img/login/login.jpg")`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
+          const moreUser = getAdditionalUserInfo(resultt)
+
+          console.log("Soy moreuserr", moreUser);
+
+          return moreUser
+        }).catch((error) => {
+          // Manejar el error
+        });
+      redirectWorld(profile)
+    }
+    fetchData();
+  }
+}, []);
+
+return (
+  <div
+    className='flex items-center w-full h-screen'
+    style={{
+      backgroundImage: `url("/img/login/login.jpg")`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}
+  >
+    {/* <img src='/img/login/login.png' alt='Fondo De Inicio de Sesión' /> */}
+    <form
+      onSubmit={handleSubmit}
+      className='w-full sm:w-1/2 max-w-[1000px] flex justify-center flex-col p-8 sm:ml-20 bg-sin_derechos bg-opacity-75 text-amarillito text-left rounded shadow-lg'
+      style={{ height: formHeight }}
     >
-      {/* <img src='/img/login/login.png' alt='Fondo De Inicio de Sesión' /> */}
-      <form
-        onSubmit={handleSubmit}
-        className='w-full sm:w-1/2 max-w-[1000px] flex justify-center flex-col p-8 sm:ml-20 bg-sin_derechos bg-opacity-75 text-amarillito text-left rounded shadow-lg'
-        style={{ height: formHeight }}
-      >
-        <h2 className='text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto'>Iniciar sesión</h2>
-        <div className='flex mb-4'>
-          <IoMailSharp className=' w-10 h-10 mr-2 mt-2 text-amarillito' />
+      <h2 className='text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto'>Iniciar sesión</h2>
+      <div className='flex mb-4'>
+        <IoMailSharp className=' w-10 h-10 mr-2 mt-2 text-amarillito' />
 
-          <input
-            type='email'
-            id='email'
-            placeholder='Correo electrónico'
-            value={email}
-            onChange={handleEmailChange}
-            className='text-xl lg:text-3xl w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-xl placeholder:lg:text-3xl placeholder:font-light'
-            required
-          />
+        <input
+          type='email'
+          id='email'
+          placeholder='Correo electrónico'
+          value={email}
+          onChange={handleEmailChange}
+          className='text-xl lg:text-3xl w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-xl placeholder:lg:text-3xl placeholder:font-light'
+          required
+        />
+      </div>
+      <div className='flex mb-4'>
+        <AiFillUnlock className='w-10 h-10 mr-2 mt-2  text-amarillito' />
+
+        <input
+          type='password'
+          id='password'
+          placeholder='Contraseña'
+          value={password}
+          onChange={handlePasswordChange}
+          className='text-xl lg:text-3xl w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-xl placeholder:lg:text-3xl placeholder:font-light '
+          required
+        />
+      </div>
+
+      {isLoading ? (
+        <div className='flex w-full justify-center mt-4'>
+          <Loading />
         </div>
-        <div className='flex mb-4'>
-          <AiFillUnlock className='w-10 h-10 mr-2 mt-2  text-amarillito' />
+      ) : null}
 
-          <input
-            type='password'
-            id='password'
-            placeholder='Contraseña'
-            value={password}
-            onChange={handlePasswordChange}
-            className='text-xl lg:text-3xl w-full bg-transparent border-b-4 p-4 border-amarillito placeholder:text-caca_clara rounded placeholder:text-xl placeholder:lg:text-3xl placeholder:font-light '
-            required
-          />
-        </div>
+      <div className='flex font-texto text-3xl mt-4 md:mt-[100px] xl:mt-[200px] xxl:mt-[300px]'>
+        <button className='relative'>
+          <Link href='/initialS'>
+            <img src='icons/login/back_button.svg' alt='Volver button' />
+          </Link>
+        </button>
+        <button type='submit' className='relative'>
+          <img src='icons/login/login_button.svg' alt='continuar button' />
+        </button>
+      </div>
+      <div className='relative grid justify-center w-full mt-6'>
+        <button className='relative mx-0 m-auto' type='button' onClick={handleGoogleLogin}>
+          <img src='icons/login/googleButton.png' alt='continuar button' />
+        </button>
+      </div>
+    </form>
 
-        {isLoading ? (
-          <div className='flex w-full justify-center mt-4'>
-            <Loading />
-          </div>
-        ) : null}
-
-        <div className='flex font-texto text-3xl mt-4 md:mt-[100px] xl:mt-[200px] xxl:mt-[300px]'>
-          <button className='relative'>
-            <Link href='/initialS'>
-              <img src='icons/login/back_button.svg' alt='Volver button' />
-            </Link>
-          </button>
-          <button type='submit' className='relative'>
-            <img src='icons/login/login_button.svg' alt='continuar button' />
-          </button>
-        </div>
-        <div className='relative grid justify-center w-full mt-6'>
-          <button className='relative mx-0 m-auto' type='button' onClick={handleGoogleLogin}>
-            <img src='icons/login/googleButton.png' alt='continuar button' />
-          </button>
-        </div>
-      </form>
-
-    </div>
-  )
+  </div>
+)
 }
 
 export default Login
--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -97,88 +97,90 @@ const Resgiter = () => {
   //Inicia sesión con Google
   
   const handleGoogleLogin = async () => {
-    if (typeof window !== 'undefined') {
-    const auth = getAuth();
-    // Inicia el loading
-    setIsLoading(true)
-    const { profile } =await signInWithPopup(auth, provider)
-    .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-		const moreUser = getAdditionalUserInfo(result)
+    console.log('hola')
+  //   if (typeof window !== 'undefined') {
+  //   const auth = getAuth();
+  //   // Inicia el loading
+  //   setIsLoading(true)
+  //   const { profile } =await signInWithPopup(auth, provider)
+  //   .then((result) => {
+  //   // This gives you a Google Access Token. You can use it to access the Google API.
+  //   const credential = GoogleAuthProvider.credentialFromResult(result);
+  //   const token = credential.accessToken;
+  //   // The signed-in user info.
+	// 	const moreUser = getAdditionalUserInfo(result)
     
-    //Comentario
-    console.log(moreUser);
+  //   //Comentario
+  //   console.log(moreUser);
 
-    return moreUser
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-    return error
-  });
+  //   return moreUser
+  //   // IdP data available using getAdditionalUserInfo(result)
+  //   // ...
+  // }).catch((error) => {
+  //   // Handle Errors here.
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   const email = error.customData.email;
+  //   // The AuthCredential type that was used.
+  //   const credential = GoogleAuthProvider.credentialFromError(error);
+  //   // ...
+  //   return error
+  // });
 
-    const data = {
-      name: profile.given_name,
-      last_name: profile.family_name,
-      email: profile.email,
-      password: profile.id,
-    }
+  //   const data = {
+  //     name: profile.given_name,
+  //     last_name: profile.family_name,
+  //     email: profile.email,
+  //     password: profile.id,
+  //   }
 
-    if (profile) {
-      axios
-        .post(`${apiUrl}/users/loginGoogle`, data)
-        .then((response) => {
-          console.log(response.data) // Maneja la respuesta del servidor según tus necesidades
+  //   if (profile) {
+  //     axios
+  //       .post(`${apiUrl}/users/loginGoogle`, data)
+  //       .then((response) => {
+  //         console.log(response.data) // Maneja la respuesta del servidor según tus necesidades
 
-          // Detiene el loading
-          setIsLoading(false)
+  //         // Detiene el loading
+  //         setIsLoading(false)
 
-          if (response.data.user) {
-            // Guarda el usuario en el localStorage
-            localStorage.setItem('user', JSON.stringify(response.data.user))
-          }
-          // Detiene el loading
-          setIsLoading(false)
-          // Envía al usuario a la página de inicio
-          router.push('/world')
-        })
-        .catch((error) => {
-          // console.error(error)
+  //         if (response.data.user) {
+  //           // Guarda el usuario en el localStorage
+  //           localStorage.setItem('user', JSON.stringify(response.data.user))
+  //         }
+  //         // Detiene el loading
+  //         setIsLoading(false)
+  //         // Envía al usuario a la página de inicio
+  //         router.push('/world')
+  //       })
+  //       .catch((error) => {
+  //         // console.error(error)
 
-          // Detiene el loading
-          setIsLoading(false)
+  //         // Detiene el loading
+  //         setIsLoading(false)
 
-          // Alerta de error
-          Swal.fire({
-            title: 'Error!',
-            text: 'Ocurrió un error al iniciar sesión. Verifica tus credenciales.',
-            confirmButtonText: 'Aceptar',
-            buttonsStyling: false,
-            color: '#F4DFB0',
-            iconColor: '#F4DFB0',
-            background: '#8C6F4D',
-            iconHtml: '<img src="/icons/login/advertencia.svg" alt="error" class="w-20 h-20">',
-            customClass: {
-              popup: 'rounded-3xl',
-              container: 'rounded-xl',
-              title: 'text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
-              htmlContainer: 'text-amarillito text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
-              confirmButton: 'bg-moradito_palido text-amarillito font-texto text-xl p-4 rounded',
-            },
-          })
-        })
-    }
-  }}
+  //         // Alerta de error
+  //         Swal.fire({
+  //           title: 'Error!',
+  //           text: 'Ocurrió un error al iniciar sesión. Verifica tus credenciales.',
+  //           confirmButtonText: 'Aceptar',
+  //           buttonsStyling: false,
+  //           color: '#F4DFB0',
+  //           iconColor: '#F4DFB0',
+  //           background: '#8C6F4D',
+  //           iconHtml: '<img src="/icons/login/advertencia.svg" alt="error" class="w-20 h-20">',
+  //           customClass: {
+  //             popup: 'rounded-3xl',
+  //             container: 'rounded-xl',
+  //             title: 'text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
+  //             htmlContainer: 'text-amarillito text-3xl md:text-4xl xl:text-7xl font-bold mb-11 text-left font-texto',
+  //             confirmButton: 'bg-moradito_palido text-amarillito font-texto text-xl p-4 rounded',
+  //           },
+  //         })
+  //       })
+  //   }
+  // }
+  }
 
   useEffect(() => {
     const screenHeight = window.innerHeight
